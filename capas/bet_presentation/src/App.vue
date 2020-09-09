a8ee8d46400c9a0eab893363517c9343863caa1e
jholkin <jhil.palacios@unas.edu.pe>
2020-09-09 01:03:59 -0500

    bet_backend api expone dos recursos

--- capas/bet_presentation/src/App.vue
+++ capas/bet_presentation/src/App.vue
@@ -1,32 +1,86 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <template>
+        <b-navbar>
+          <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <img
+                src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                alt="Lightweight UI components for Vue.js based on Bulma"
+              />
+            </b-navbar-item>
+          </template>
+          <template slot="start">
+            <b-navbar-item href="#">
+              Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+              Documentation
+            </b-navbar-item>
+            <b-navbar-dropdown label="Info">
+              <b-navbar-item href="#">
+                About
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                Contact
+              </b-navbar-item>
+            </b-navbar-dropdown>
+          </template>
+
+          <template slot="end">
+            <b-navbar-item tag="div">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </b-navbar-item>
+          </template>
+        </b-navbar>
+      </template>
+      <SearchGames></SearchGames>
+    </div>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Component
+        </p>
+        <a href="#" class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </a>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris.
+          <a href="#">@bulmaio</a>. <a href="#">#css</a>
+          <a href="#">#responsive</a>
+          <br />
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
     </div>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SearchGames from "./components/SearchGames";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    SearchGames
+  }
+};
+</script>
