6c0aee27ac2ac6d9ab5e0bae0293cb2520f65182
jholkin <jhil.palacios@unas.edu.pe>
2020-10-22 09:42:49 -0500

    modules

---
+++ microkernel/laravel_modules/Modules/SecondModule/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('secondmodule::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('secondmodule.name') !!}
+    </p>
+@endsection
---
+++ microkernel/plugins/testCalculator/testCalculator.js
@@ -0,0 +1,3 @@
+const cal = require('calculator-jholkin');
+
+console.log(cal.suma(1,2));
