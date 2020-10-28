59d297744bf4e84fe3588fc837e12ae7e0b1f275
jholkin <jhil.palacios@unas.edu.pe>
2020-10-27 23:52:56 -0500

    add laravel_modules_v2

---
+++ microkernel/laravel_modules_v2/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/info', function() {
+    return "<h1>Bienvenido extraño, Este es el core del microkernel poderoso....!!!</h1>";
+});
--- microkernel/plugins/testCalculator/testCalculator.js
+++
@@ -1,3 +0,0 @@
-const cal = require('calculator-jholkin');
-
-console.log(cal.suma(1,2));
